41f45c6b903be3fe139280e702ef2dd20a0fd013
Sam Tureski <gpiggies@gmail.com>
2018-04-08 16:23:32 +0200

    update commit; still needs fixes

--- process_data.py
+++ process_data.py
@@ -6,12 +6,14 @@
 import praatTextGrid
 from pydub import AudioSegment
 import os
+import sys
+#import praatUtil
 import scipy.io.wavfile as wav
 
-(rate,sig) = wav.read("file.wav")
 
 AudioSegment.ffmpeg = "/opt/local/var/macports/sources/rsync.macports.org/macports/release/tarballs/ports/multimedia/ffmpeg"
 
+#from Daniel de Kok's Deep Learning Course
 class Numberer:
     def __init__(self):
         self.v2n = dict()
@@ -36,71 +38,109 @@ class Numberer:
     def max_number(self):
         return len(self.n2v) + 1
 
-def process_textgrid_and_wav():
+def process_textgrid_and_wav(textgrid_path):
     #textgrid_path, wav_path
     # path that contains all the annotated wave files
     path = '/Users/samski/Documents/Textgrids_for_Model'
 
-    for f_name in os.listdir(path):
+    #ADD ERROR HANDLING
+    try:
 
-            all_times_of_clips = []
-            # instantiate a new TextGrid object
-            textGrid = praatTextGrid.PraatTextGrid(0, 0)
+        for f_name in os.listdir(path):
 
-            arrTiers = textGrid.readFromFile('/Users/samski/Documents/Textgrids_for_Model/' + f_name)
-            file_name_wo_extension = os.path.splitext(os.path.basename(f_name))[0]
-            wav_path = '/Users/samski/Documents/Textgrids_for_Model/' + '.wav'
-            numTiers = len(arrTiers)
-            print(numTiers)
-            # if numTiers != 2:
-            #     raise Exception("we expect two tiers in this file")
+                all_times_of_clips = []
+                # instantiate a new TextGrid object
+                textGrid = praatTextGrid.PraatTextGrid(0, 0)
 
-            #use segments tier
-            tier = arrTiers[1]
+                arrTiers = textGrid.readFromFile('/Users/samski/Documents/Textgrids_for_Model/' + f_name)
 
-            for i in range(tier.getSize()):
-                clip_start_and_end = []
-                #interval is list of start time, end time, segment annotation, in that order
-                interval = tier.get(i)
-                if tier.getSize() <= 1:
-                    #ADD this later
-                    interval[2] = "NONE"
-                    #get_sound_clips(wav_path,interval[0],interval[1])
-                    #print("\t", interval[2])
+                numTiers = len(arrTiers)
+                print(numTiers)
+                if numTiers != 2:
+                    raise Exception("we expect two tiers in this file")
 
+                #use segments tier
+                tier = arrTiers[1]
 
-                clip_start_and_end.append(interval[0])
-                clip_start_and_end.append(interval[1])
-                #print(clip_start_and_end)
+                for i in range(tier.getSize()):
+                    clip_start_and_end = []
+                    #interval is list of start time, end time, segment annotation, in that order
+                    interval = tier.get(i)
+                    if tier.getSize() <= 1:
+                        #ADD this later
+                        interval[2] = "NONE"
+                        #get_sound_clips(wav_path,interval[0],interval[1])
+                        #print("\t", interval[2])
 
 
-def get_sound_clips(path, clip_times):
+                    clip_start_and_end.append(interval[0])
+                    clip_start_and_end.append(interval[1])
 
-    song = AudioSegment.from_wav("/Users/samski/Documents/Wavs_for_Model/rec_006_IS_id_011_1.wav")
-    #song = AudioSegment.from_wav(path)
+                    all_times_of_clips.append(clip_start_and_end)
 
-    for clip_time in clip_times:
+                file_name_without_extension = os.path.splitext(os.path.basename(f_name))[0]
+                wav_path = '/Users/samski/Documents/Wavs_for_Model/' + file_name_without_extension + "_band.wav"
+                get_sound_clips(wav_path, all_times_of_clips)
 
-        start = clip_time[0]
-        end = clip_time[1]
+    except OSError:
+    # If directory has already been created or is inaccessible
+        if not os.path.exists(path):
+            sys.exit("Error opening given textgrid file path")
 
-        start_time_in_ms = start * 1000
-        end_time_in_ms = end * 1000
+    return all_times_of_clips
 
-        first_10_seconds = song[start_time_in_ms:end_time_in_ms ]
 
-    (rate, sig) = wav.read("file.wav")
-    mfcc_feat = mfcc(sig, rate)
-    fbank_feat = logfbank(sig, rate)
+def get_sound_clips(wav_path, clip_times, already_filtered=True):
 
-    #backwards = song.reverse()
-    #print(first_10_seconds)
+    #filter out irrelevant frequencies!!!!!!!!!!!!!!!
+    try:
+        song = AudioSegment.from_wav(wav_path)
+        wav_name_without_extension = os.path.splitext(os.path.basename(f_name))[0]
 
-    first_10_seconds.export("/Users/samski/Documents/Wavs_for_Model/whaaat.wav", format="wav")
+        #song = AudioSegment.from_wav(path)
+
+        # if not already_filtered:
+        #     praatUtil.applyBandPassFilter(wav_path, 50,20000,20)
+
+        for clip_time in clip_times:
+
+            start = clip_time[0]
+            end = clip_time[1]
+
+            start_time_in_ms = start * 1000
+            end_time_in_ms = end * 1000
+
+            phoneme_segment = song[start_time_in_ms:end_time_in_ms]
+
+        # (rate, sig) = wav.read("file.wav")
+        # mfcc_feat = mfcc(sig, rate)
+        # fbank_feat = logfbank(sig, rate)
+
+
+            wav_name = wav_name_without_extension + start + ".wav"
+            phoneme_segment.export("/Users/samski/Documents/Wavs_for_Model/" + wav_name, format="wav")
+
+    except OSError:
+        # If directory has already been created or is inaccessible
+        if not os.path.exists(path):
+            sys.exit("Error opening wave file given path")
+    return
+
+
+def get_mfccs():
     return
 
 
 if '__main__' == __name__:
-   # get_sound_clips()
-   process_textgrid_and_wav()
+    #  MAKE SURE TO ADD DEFAULT FILTERED
+    #remove hardcoded paths later
+    textgrid_path = '/Users/samski/Documents/Textgrids_for_Model'
+    #wav_path = "/Users/samski/Documents/Wavs_for_Model/rec_006_IS_id_011_1.wav"
+
+    clip_times = process_textgrid_and_wav(textgrid_path)
+    # clip_times = [[10, 15]]
+
+    #
+    #get_sound_clips(wav_path, clip_times)
+
 
